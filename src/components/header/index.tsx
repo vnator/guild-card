29913f48b3e897da0bd1f7b9fca3c7da2095d3d9
João Henrique Serodio <joao.serodio@vnator.com>
2023-08-04 12:10:50 -0300

    feat: header and picture

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,18 @@
 import { Container } from "../container";
 
 import style from './header.module.css'
+import profile from '../../assets/profile.jpeg'
 
 export const Header = () => (
   <header className={style.header}>
     <Container>
       <div>
         <div className={style.background} />
-        <div className={style.content}>cabeçalho</div>
+        <div className={style.content}>
+          <picture className={style.picture}>
+            <img src={profile.src} alt='João Henrique Serodio' />
+          </picture>
+        </div>
       </div>
     </Container>
   </header>
