3ce35b288cb7a54eed9e7c7b6516986063dd6e83
João Henrique Serodio <joao.serodio@vnator.com>
2023-08-25 16:15:35 -0300

    feat: add index page and media screen mobile

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,5 +3,5 @@ export const COMPANY_RESPONSABLE='Employer Responsable'
 export const COMPANY_POSITION='Software Engineer'
 export const ME_EMAIL="joao.serodio@vnator.com"
 export const ME_GITHUB="https://github.com/jhserodio"
-export const ME_LINKEDIN="https://www.linkedin.com/in/jh-serodio"
+export const ME_LINKEDIN="https://www.linkedin.com/in/jhserodio"
 export const ME_PHONE="+55 (42) 99999-3057"
