f3ef5234db234a8d8bba0c825d70082c6ee6b9df
João Henrique Serodio <joao.serodio@vnator.com>
2023-08-28 14:04:23 -0300

    chore: ci-cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://serodio.github.io',
+  base: '/',
   integrations: [react()],
   experimental: {
     assets: true,
