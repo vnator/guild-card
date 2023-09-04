dedbd1345670081b34e70d7e965fafb35f877d7e
João Henrique Serodio <joao.serodio@vnator.com>
2023-09-04 12:22:37 -0300

    feat: update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jhserodio.github.io',
-  base: '.',
+  base: './',
   integrations: [react()],
-  experimental: {
-    assets: true,
-  }
 });
